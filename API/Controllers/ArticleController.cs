76abd12e8f82aa1924525346cd11dc4ef3064aba
Kyle Wagner <70072537+kylemwagner83@users.noreply.github.com>
2021-08-09 15:44:37 -0600

    Add POST Method

--- API/Controllers/ArticleController.cs
+++ API/Controllers/ArticleController.cs
@@ -8,16 +8,21 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ArticleController : ControllerBase
     {
+        MySqlConnection conn;
+        public ArticleController()
+        {
+            string connStr = "server=localhost;user=root;database=library;port=3306;password=Pa$$w0rd";
+            conn = new MySqlConnection(connStr);
+        }
+
         [HttpGet("{id}")]
         public string GetArticle(int id)
         {
-            string connStr = "server=localhost;user=root;database=library;port=3306;password=Pa$$w0rd";
-            MySqlConnection conn = new MySqlConnection(connStr);
             string articleData = "Data not retrieved";
             try
             {
                 conn.Open();
-                string sql = $"SELECT ArticleData FROM Article where Id = {id}";
+                string sql = $"SELECT ArticleData FROM Article WHERE Id = {id}";
                 MySqlCommand cmd = new MySqlCommand(sql, conn);
                 MySqlDataReader rdr = cmd.ExecuteReader();
                 while (rdr.Read())
@@ -34,10 +39,23 @@ namespace API.Controllers
             return articleData;
         }
 
-        [HttpPost]
-        public void SaveArticle()
+        [HttpPost("{id}")]
+        public string SaveArticle(int id)
         {
-
+            try
+            {
+                int randNum = new Random().Next(1, 100);
+                conn.Open();
+                string sql = $"UPDATE Article SET ArticleData = '{randNum}' WHERE Id = {id}";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            conn.Close();
+            return "Updated article";
         }
 
     }
